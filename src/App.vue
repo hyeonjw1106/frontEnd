b78b53d0754e0e2058e353a3e808b90ad904e3ca
현준우 <junjuns1106hyun@gmail.com>
2026-04-03 12:35:09 +0900

    인스타그램

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="header">
+      <ul class="header-button-left">
+        <li>Cancel</li>
+      </ul>
+      <ul class="header-button-right">
+        <li>Next</li>
+      </ul>
+      <img src="./assets/logo.svg" class="logo" />
+    </div>
+
+    <Container :posts="posts" />
+    <button @click="more">더보기</button>
+
+    <div class="footer">
+      <ul class="footer-button-plus">
+        <input type="file" id="file" class="inputfile" />
+        <label for="file" class="input-plus">+</label>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Container from './components/Container.vue';
+import posts from './Post.js'; 
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  },
+  data() {
+    return {
+      count : 0,
+      posts
+    };
+  },
+  methods:{
+    more() {
+      this.count++;
+      axios.get('https://qkrwpgus.github.io/vue/more' + this.count + '.json')
+      .then((result)=>{
+        this.posts.push(...result.data);
+      })  
+    }
+  }
+};
+</script>
---
+++ src/components/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <img :src="post.userImage" class="profile" />
+      <span class="profile-name">{{ post.name }}</span>
+    </div>
+
+    <div class="post-body">
+      <img :src="post.postImage" class="post-img" />
+    </div>
+
+    <div class="post-content">
+      <p>{{ post.likes }} Likes</p>
+      <p>
+        <strong>{{ post.name }}</strong> {{ post.content }}
+      </p>
+      <p class="date">{{ post.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    post: Object
+  }
+}
+</script>
